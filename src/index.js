db4efb22d0ba68ec852083d71e36c91903a0d29f
eGluZl <im.hxy@outlook.com>
2020-07-30 22:10:44 +0800

    pool connect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const {Pool} = require('pg')
 
 const config = {
-    user: 'egluzl',
     host: 'localhost',
-    database: 'poetry',
-    password: 'pg525300',
     port: 5432,
+    database: 'poetry',
+    user: 'egluzl',
+    password: 'pg525300',
+
+    connectionTimeoutMillis: 30000,
+    max: 20,
+    idleTimeoutMillis: 30000,
 }
 
+const pool = new Pool(config)
+
+pool.connect().then(client => {
+    client.query('select now()').then(res => {
+        console.log(res)
+        client.release()
+    }).catch(err => {
+        console.error(err)
+        client.release()
+    })
+})
 
